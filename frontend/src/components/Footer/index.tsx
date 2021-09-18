7d6fdbe1fdb861e5c4ea486267d2b2ae0bff55c3
Dwj <almeida.jenyy@gmail.com>
2021-09-17 21:00:39 -0300

    Componentes Basicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Desenvolvido <a href="https://github.com/Jenyll" target="_blank" rel="noreferrer">Jenyelle</a></p>
+                <p className="text-light"><small><strong>Lorem ipsum dolor sit amet</strong><br />
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt <a href="https://www.instagram.com/dwendyy/" target="_blank" rel="noreferrer">@dwendyy</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
